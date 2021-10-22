18718e0bbed00836da2362ae581d6ccfec1f1d8f
TheDeafOne <Woodburnkb20@gcc.edu>
2021-10-22 11:20:22 -0400

    tab1 onInit setup

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
-
+export class Tab1Page implements OnInit, AfterViewInit {
+  ngOnInit(){}
+  ngAfterViewInit(){}
   constructor() {}
 
 }
