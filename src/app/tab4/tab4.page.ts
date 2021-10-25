0cece64d23f9a17df7565fdebd2b2787dbe7d308
TheDeafOne <Woodburnkb20@gcc.edu>
2021-10-25 14:34:21 -0400

    Added all tabs

---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: 'tab4.page.html',
+  styleUrls: ['tab4.page.scss']
+})
+export class Tab4Page implements OnInit, AfterViewInit {
+  ngOnInit(){}
+  ngAfterViewInit(){}
+  constructor() {}
+
+}
---
+++ src/app/tab5/tab5.page.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-tab5',
+  templateUrl: 'tab5.page.html',
+  styleUrls: ['tab5.page.scss']
+})
+export class Tab5Page implements OnInit, AfterViewInit {
+  ngOnInit(){}
+  ngAfterViewInit(){}
+  constructor() {}
+
+}
