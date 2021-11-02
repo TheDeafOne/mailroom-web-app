cffb1a66c840c5294d1e0de7b1501070fdd11ffe
TheDeafOne <Woodburnkb20@gcc.edu>
2021-11-02 16:08:22 -0400

    img saving start

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, HostListener, ɵɵngDeclareFactory } from '@angular/core';
 import SignaturePad from 'signature_pad';
 import { Base64ToGallery } from '@ionic-native/base64-to-gallery/ngx';
-@
-Component({
+import { O_DIRECTORY } from 'constants';
+
+@Component({
   selector: 'app-tab5',
   templateUrl: 'tab5.page.html',
   styleUrls: ['tab5.page.scss']
@@ -42,12 +43,23 @@ export class Tab5Page implements OnInit, AfterViewInit {
     }
   }
   save(): void {
+    
     const img = this.signaturePad.toDataURL();
+    console.log(typeof img)
+
+    const fileName = new Date().getTime() + '.jpeg';
+    // const savedFile = await FileSystem.writeFile({
+    //   path: fileName,
+    //   data: img,
+    // });
+    // change cause important for web specific platforms
     this.base64ToGallery.base64ToGallery(img).then(
       res => console.log('Saved image to gallery ', res),
       err => console.log('Error saving image to gallery ', err)
     );
   }
+
+
   clear() {
     this.signaturePad.clear();
   }
