31699d9dbeba26dbb2b2c9cb9cdd92322d1bcdab
TheDeafOne <Woodburnkb20@gcc.edu>
2021-11-09 16:40:33 -0500

    ng search filter and json parsing with ES6 imports

--- src/app/tab4/tab4.module.ts
+++ src/app/tab4/tab4.module.ts
@@ -4,16 +4,16 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab4Page } from './tab4.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { Tab4PageRoutingModule } from './tab4-routing.module';
-
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab4PageRoutingModule
+    Tab4PageRoutingModule,
+    Ng2SearchPipeModule
   ],
   declarations: [Tab4Page]
 })
--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import data from 'src/assets/data/test-data.json';
+
+console.log(data)
 
 @Component({
   selector: 'app-tab4',
   templateUrl: 'tab4.page.html',
   styleUrls: ['tab4.page.scss']
 })
+
 export class Tab4Page implements OnInit, AfterViewInit {
+  filterTerm: string;
+
   ngOnInit(){}
   ngAfterViewInit(){}
   constructor() {}
+
+  
 
 }
