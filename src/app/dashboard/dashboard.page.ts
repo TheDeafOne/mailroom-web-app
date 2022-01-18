5a48e7944b3e4e35a8ae7526fb426f339a19131d
TheDeafOne <woodburnkb20@gcc.edu>
2022-01-17 21:07:44 -0500

    code cleaning

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,39 +1,22 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
-import { Chart, DatasetController, registerables } from 'chart.js';
+import { Chart, registerables } from 'chart.js';
 import { MenuController, AlertController } from '@ionic/angular';
 
 import data from 'src/assets/data/test-data.json';
 
 var daysLong = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 var createdOnData: any = {};
-var signedOnData: any = {};
 var chartDisplayDataOne = [];
 var labelsG = [];
-var enteredValue = 0;
-var signedValue = 0;
 
 
 function sortStudData() {
   for(const inEl of data){
-    enteredValue++;
     let cDate: string = new Date(inEl["CreatedOn"]).toDateString();
-    let sDate: string = new Date(inEl["SignedOn"]).toDateString();
-    
-    // adding created on data
     if (cDate in createdOnData){
       createdOnData[cDate].push(inEl);
     } else {
       createdOnData[cDate] = [inEl];
-    }
-    
-    // adding signed on data
-    if (sDate != "NA"){
-      signedValue++;
-      if (sDate in signedOnData){
-        signedOnData[sDate].push(inEl);
-      } else {
-        signedOnData[sDate] = [inEl];
-      }
     }
   }
 }
@@ -44,7 +27,36 @@ function clearChartXY(){
 }
 
 
+function defaultChartDisplay(){
+  let dayHours = {};
+    chartDisplayDataOne = [];
+    labelsG = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
+
+    // get latest day
+    let tmpKDates = Object.keys(createdOnData);
+    let tDay = createdOnData[tmpKDates[tmpKDates.length-1]];
+    // extract data from 
+    for (const val in tDay){
+      let time = new Date(tDay[val]["CreatedOn"]).getHours();
+      if (time in dayHours){
+        dayHours[time].push(tDay[val]);
+      } else {
+        dayHours[time] = [tDay[val]];
+      }
+    }
+    // change display data
+    for (const val in labelsG){
+      let time = (Number(val.substring(0,2))+6);
+      if (time in dayHours){
+        chartDisplayDataOne.push(dayHours[time].length)
+      } else {
+        chartDisplayDataOne.push(0);
+      }
+    }
+}
+
 sortStudData();
+defaultChartDisplay();
 
 Chart.register(...registerables)
 
@@ -65,10 +77,9 @@ export class dashboard implements OnInit {
 
 
   ngOnInit() {
-    this.oneDay();
-    document.getElementById("entered").innerText = enteredValue.toString();
-    document.getElementById("signed").innerText = signedValue.toString();
-    document.getElementById("inSystem").innerText = (enteredValue-signedValue).toString();
+    // document.getElementById("entered").innerText = enteredValue.toString();
+    // document.getElementById("signed").innerText = signedValue.toString();
+    // document.getElementById("inSystem").innerText = (enteredValue-signedValue).toString();
     
     // console.log(document.getElementById("dayFilter").children);
   }
@@ -409,31 +420,7 @@ export class dashboard implements OnInit {
   oneDay(){
     (<HTMLInputElement> document.getElementById("one-day-filter")).disabled = true;
     clearChartXY();
-    let dayHours = {};
-    chartDisplayDataOne = [];
-    labelsG = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
-
-    // get latest day
-    let tmpKDates = Object.keys(createdOnData);
-    let tDay = createdOnData[tmpKDates[tmpKDates.length-1]];
-    // extract data from 
-    for (const val in tDay){
-      let time = new Date(tDay[val]["CreatedOn"]).getHours();
-      if (time in dayHours){
-        dayHours[time].push(tDay[val]);
-      } else {
-        dayHours[time] = [tDay[val]];
-      }
-    }
-    // change display data
-    for (const val in labelsG){
-      let time = (Number(val.substring(0,2))+6);
-      if (time in dayHours){
-        chartDisplayDataOne.push(dayHours[time].length)
-      } else {
-        chartDisplayDataOne.push(0);
-      }
-    }
+    defaultChartDisplay();
     this.chart.destroy();
     this.createChart();
   }
